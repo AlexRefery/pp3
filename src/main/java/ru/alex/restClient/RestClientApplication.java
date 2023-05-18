d15643df57e15999330f49e04126ab64a732bb8b
shamas <shamas@tavrida.com>
2023-05-18 15:45:58 +0300

    Не работает Delete. Даже не знаю, куда копать.

--- src/main/java/ru/alex/restClient/RestClientApplication.java
+++ src/main/java/ru/alex/restClient/RestClientApplication.java
@@ -2,39 +2,44 @@ package ru.alex.restClient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 import ru.alex.restClient.pojo.User;
 
-import java.net.HttpCookie;
-import java.util.Optional;
-
-import static com.fasterxml.jackson.databind.type.LogicalType.Collection;
-
 @SpringBootApplication
 public class RestClientApplication {
-	private final static String URL_GET_USERS = "http://94.198.50.185:7081/api/users";
+	private final static String URL_USERS = "http://94.198.50.185:7081/api/users";
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestClientApplication.class, args);
 
 		RestTemplate restTemplate = new RestTemplate();
-		ResponseEntity<User[]> responseUsers = restTemplate.getForEntity(URL_GET_USERS, User[].class);
-		User[] users = responseUsers.getBody();
-
-		System.out.println(responseUsers.getHeaders().get("Set-Cookie").get(0));
-		Optional.ofNullable(responseUsers.getHeaders().get(HttpHeaders.SET_COOKIE)).stream()
-				.flatMap(Collection::stream)
-				.map(HttpCookie::parse)
-				.flatMap(Collection::stream)
-				.map(HttpCookie::toString)
-				.forEach(c -> headers.add(HttpHeaders.COOKIE, c));
-
+		ResponseEntity<User[]> responseUsers = restTemplate.getForEntity(URL_USERS, User[].class);
+		String cookie = responseUsers.getHeaders().get("Set-Cookie").get(0);
+		System.out.println(cookie);
 		HttpHeaders headers = new HttpHeaders();
-		headers.set("", "application/json");
 
-		System.out.println("Done!");
+		headers.set(HttpHeaders.COOKIE, cookie);
+
+		HttpEntity<User> request = new HttpEntity<>(new User("James", "Brown", (byte) 22), headers);
+		String code1 = restTemplate.exchange(URL_USERS, HttpMethod.POST, request, String.class).getBody();
+//		String code1 = restTemplate.postForEntity(URL_USERS, request, String.class).getBody();
+		System.out.println(code1);
+
+		Long id = 3L;
+		User updatedUser = new User("Thomas", "Shelby", (byte) 33);
+		HttpEntity<User> requestUpdate = new HttpEntity<>(updatedUser, headers);
+		String code2 = restTemplate.exchange(URL_USERS, HttpMethod.PUT, requestUpdate, String.class, id).getBody();
+		System.out.println(code2);
+
+		headers.set(HttpHeaders.COOKIE, cookie.replace("/", "/3"));
+		HttpEntity<User> requestDelete = new HttpEntity<>(headers);
+		System.out.println(headers);
+
+		ResponseEntity<String> response = restTemplate.exchange(URL_USERS + "/" + id, HttpMethod.DELETE, requestDelete, String.class);
+		String code3 = response.getBody();
+
+		System.out.println(code1 + " + " + code2 + " + " + code3 + " + " + "Done!");
 	}
 
 }
--- src/main/java/ru/alex/restClient/pojo/User.java
+++ src/main/java/ru/alex/restClient/pojo/User.java
@@ -3,5 +3,9 @@ package ru.alex.restClient.pojo;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record User (Long id, String name, String lastName, Byte age) {}
+public record User (Long id, String name, String lastName, Byte age) {
+    public User (String name, String lastName, Byte age) {
+        this(0L, name, lastName, age);
+    }
+}
 
